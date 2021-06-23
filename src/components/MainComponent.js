5369f3ec4c3f93e85fd14123e6fded00025f1064
piyush-Rathor <psrathor16072000@gmail.com>
2021-06-23 15:40:48 +0530

    Ui Update

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,22 +1,23 @@
-import react, { Component } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import AllVideosComponent from "./AllVideosComponent";
 import UploadVideoComponent from "./UploadVideoComponent";
-class Main extends Component {
-  componentDidMount() {}
-
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={AllVideosComponent} />
-          <Route exact path="/upload" component={UploadVideoComponent} />
-          <Redirect to="/" />
-        </Switch>
-      </Router>
-    );
-  }
-}
+const Main = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" >
+          <AllVideosComponent/>
+        </Route>
+        <Route exact path="/upload">
+        <UploadVideoComponent/>
+        </Route>
+        <Route exact path="/">
+        <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default Main;
--- src/components/UploadVideoComponent.js
+++ src/components/UploadVideoComponent.js
@@ -1,57 +1,126 @@
 import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
-const uploadVideo = (props) => (
-  <Container>
-    <Card className="m-auto mt-5" style={{ "max-width": "45rem" }}>
-      <Card.Body>
-        <Form>
-          <Row>
-            <Col md={4}>
-              <div className="text-center">
-                <i class="fa fa-upload fa-3x text-primary m-2" aria-hidden="true"></i>
-                <h4>Drag and drop here to upload</h4>
-                <p>or</p>
-                <label for="uploadVideo">
-                  <div class="btn btn-secondary">Choose File</div>
-                </label>
-                <input type="file" className="form-control" id="uploadVideo" name="video" hidden />
-                <hr />
-                <div class="small text-start ms-3">
-                  <span>Upload Guidlines</span>
-                  <ul className="ps-3">
-                    <li>Max file 25mb</li>
-                    <li>Video length: 06-60sec</li>
-                    <li>MP4 format supported</li>
-                    <li>Video ratio 9:16</li>
-                  </ul>
-                </div>
-              </div>
-            </Col>
-            <Col md={8}>
-              <div className="">
-                <h4>Video Details</h4>
-                <hr />
-                <Form.Group className="mb-3 mt-4" controlId="formBasicEmail">
-                  <Form.Label>
-                    Caption <span className="text-danger">*</span>
-                  </Form.Label>
-                  <Form.Control type="text" />
-                  <Form.Text className="text-muted">Use a suitable caption for your video</Form.Text>
-                </Form.Group>
-                <div className="text-end">
-                  <Button variant="primary" type="submit">
-                    Upload
-                  </Button>
-                </div>
-                <div className="mt-2">
-                  <small className="text-muted p-3">By posting, I agree to Firework community guidlines & terms</small>
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </Form>
-      </Card.Body>
-    </Card>
-  </Container>
-);
+import React, { useState } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
 
-export default uploadVideo;
+const UploadVideo = () => {
+  const [videoName, setVedioName] = useState(null);
+  const [videoFile, setVedioFile] = useState(null);
+  const NameHandler = (value) => {
+    if (value.length > 0) {
+      setVedioName(value);
+      document.getElementById("nameSpan").innerHTML = "";
+    }
+    if (value.length <= 0) {
+      setVedioName(null);
+      document.getElementById("nameSpan").innerHTML =
+        "***Please Enter a Valid Movie Name/Caption***";
+    }
+  };
+  const fileHandler = (file) => {
+    setVedioFile(file);
+    document.getElementById("fileSpan").innerHTML="";
+  };
+  const formSubmitHandler=async(e)=>{
+    e.preventDefault();
+    console.log("Api Calling");
+    let formData = new FormData();
+      formData.append('movie',videoFile);
+      formData.append('movieName',videoName);
+    const response = await axios({
+      url:'http://localhost:8080/movieupload',
+      data:formData,
+      method:"POST",
+      headers: {
+        "Content-Type": "multipart/form-data",
+        "Access-Control-Allow-Origin":"*",
+      },
+    });
+    console.log(response)
+    if(response.data==="Please Upload 6-60 sec videos..."){
+      document.getElementById("fileSpan").innerHTML="Please Upload 6-60 sec video...";
+      setVedioName(null)
+    }
+    else if(response.status===200){
+      console.log(`Its working`);
+      <Redirect to="/" />
+    }
+  }
+  return (
+    <Container>
+      <Card
+        className="m-auto"
+        style={{ maxWidth: "45rem", display: "fixed", top: "200px" }}
+      >
+        <Card.Body>
+          <Form>
+            <Row>
+              <Col md={4}>
+                <div className="text-center border rounded">
+                  <i
+                    className="fa fa-upload fa-3x text-primary m-2"
+                    aria-hidden="true"
+                  ></i>
+                  <h4>upload Here</h4>
+                  <p>or</p>
+                  <label htmlFor="uploadVideo">
+                    <div className="btn btn-secondary">Choose File</div>
+                  </label>
+                  <input
+                    type="file"
+                    className="form-control"
+                    id="uploadVideo"
+                    name="video"
+                    onChange={(e) => fileHandler(e.target.files[0])}
+                    accept="video/*"
+                    hidden
+                  />
+                  <p className="text-danger text-center text-sm" id="fileSpan"></p>
+                  <hr />
+                  <div className="small text-start ms-3">
+                    <span>Upload Guidlines</span>
+                    <ul className="ps-3">
+                      <li>Video length: 06-60sec</li>
+                      <li>MP4 format supported</li>
+                      <li>Video ratio 9:16</li>
+                    </ul>
+                  </div>
+                </div>
+              </Col>
+              <Col md={8}>
+                <div className="">
+                  <h4>Video Details</h4>
+                  <hr />
+                  <Form.Group className="mb-3 mt-4" controlId="formBasicEmail">
+                    <Form.Label>
+                      Caption <span className="text-danger">*</span>
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      onChange={(e) => NameHandler(e.target.value)}
+                    />
+                    <Form.Text className="text-muted">
+                      Use a suitable caption for your video
+                    </Form.Text>
+                    <p className="text-danger text-center" id="nameSpan"></p>
+                  </Form.Group>
+                  <div className="text-end">
+                    <Button
+                      variant="primary"
+                      disabled={videoName && videoFile ? false : true}
+                      onClick={e=>formSubmitHandler(e)}
+                    >
+                      Upload
+                    </Button>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+export default React.memo(UploadVideo);
