becac21f592a18f6a91f75b1eb14646d8aa8486d
Abhineet Verma <198.abhineet@gmail.com>
2021-06-23 15:06:48 +0530

    Added VideoList Component

--- src/components/AllVideosComponent.js
+++ src/components/AllVideosComponent.js
@@ -1,8 +1,46 @@
+import { Card, Container, Row, Col, Dropdown } from "react-bootstrap";
 
-const allVideos = (props)=>(
-  <>
-    All Videos
-  </>
+const RenderVideoCard = ({ video }) => (
+  <Card style={{ maxWidth: "20rem" }}>
+    <div className="d-flex">
+      <a href={video.link} target="_blank">
+        <Card.Img variant="top" width="100%" src={video.thumbnail} alt="Video Thumbnail" className="m-auto" />
+      </a>
+    </div>
+    <Card.Body>
+      <Card.Title className="d-flex justify-content-between align-items-center">
+        <h4 className="m-0">{video.name}</h4>
+        <Dropdown>
+          <Dropdown.Toggle bsPrefix="noicon" variant="light text-black">
+            <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href={video.link}>Open</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Delete</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Card.Title>
+    </Card.Body>
+  </Card>
 );
+
+const allVideos = ({ videos }) => {
+  return (
+    <Container className="my-5 m-auto" style={{ maxWidth: "45rem" }}>
+      <h1>All Videos</h1>
+      <hr />
+      <Row sm={2} md={3}>
+        {videos.map((video) => {
+          return (
+            <Col className="p-2" key={video.id}>
+              <RenderVideoCard video={video} />
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+};
 
 export default allVideos;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,10 +7,24 @@ class Main extends Component {
   componentDidMount() {}
 
   render() {
+    const videos = [
+      {
+        'id':1,
+        'name':'video1',
+        'thumbnail':'https://source.unsplash.com/400x600/?movie',
+        'link':'#'
+      },
+      {
+        'id':2,
+        'name':'video2',
+        'thumbnail':'https://source.unsplash.com/400x600/?nature',
+        'link':'#'
+      },
+    ];
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={AllVideosComponent} />
+          <Route exact path="/" component={()=><AllVideosComponent videos={videos}/>} />
           <Route exact path="/upload" component={UploadVideoComponent} />
           <Redirect to="/" />
         </Switch>
